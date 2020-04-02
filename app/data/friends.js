c25cd2e186c29df7695d3f714601f0a4985aa931
Americ Ngwije <ngwijea@yahoo.com>
2020-04-01 17:56:16 -0700

    App load

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friends = [
+	{
+		name: "Daenerys Targaryen",
+		photo: "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/daenarys-512x512.jpg/_jcr_content/renditions/cq5dam.web.260.260.jpeg",
+		scores: [5, 1, 1, 2, 1, 2, 5, 1, 5, 2]
+	},
+	{
+		name: "Cersei Lannister",
+		photo: "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/cersei-lannister-512x512.jpg/_jcr_content/renditions/cq5dam.web.260.260.jpeg",
+		scores: [5, 4, 1, 5, 4, 5, 5, 5, 5, 3]
+	},
+	{
+		name: "Sansa Stark",
+		photo: "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/sansa-stark-512x512.jpg/_jcr_content/renditions/cq5dam.web.260.260.jpeg",
+		scores: [4, 2, 3, 4, 3, 5, 5, 5, 2, 1]
+	},
+	{
+		name: "Arya Stark",
+		photo: "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/arya-stark-512x512.jpg/_jcr_content/renditions/cq5dam.web.260.260.jpeg",
+		scores: [5, 5, 1, 5, 5, 5, 5, 5, 1, 5]
+	},
+];
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,35 @@
+var friendFind = require('../data/friends.js');
+
+module.exports = function(app) {
+
+// A GET to display a JSON of all possible friends
+    app.get("/api/friends", function(req, res) {
+      res.json(friendFind);
+    });
+  
+    // A POST to handle incoming survey results and check for compatibility.
+    app.post("/api/friends", function(req, res) {
+
+var user = req.body
+var mostCompatible = {}
+var mostCompatibleScore = 100;
+
+for (var i = 0; i < friendFind.length; i++) {
+  var closeness = 0;
+
+  for (var i2 = 0; i2 < user.scores.length; i2++) {
+      totalDiff += Math.abs(user.scores[i2] - friendFind[i].scores[j]);
+  }
+
+  if (closeness < mostCompatibleScore) {
+    mostCompatible = friendFind[i];
+    mostCompatibleScore = closeness;
+  }
+}
+
+friendFind.push(user);
+res.json(mostCompatible)
+
+});
+
+};
